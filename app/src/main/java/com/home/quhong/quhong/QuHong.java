de184e7fcc034de7c3d5221934014f8542998180
aserbao <abybxc@163.com>
2017-02-18 10:02:50 +0800

    初始框架搭建

---
+++ app/src/main/java/com/home/quhong/quhong/QuHong.java
@@ -0,0 +1,13 @@
+package com.home.quhong.quhong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuHong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qu_hong);
+    }
+}
