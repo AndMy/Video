980ff3d23fdfb0d9d800848318614d4eed573038
aserbao <abybxc@163.com>
2017-02-18 11:23:15 +0800

    RadioGroup和Fragment的联动完成（RadioGroup and the linkage of the fragments）

--- app/src/main/java/com/home/quhong/quhong/QuHong.java
+++ app/src/main/java/com/home/quhong/quhong/QuHong.java
@@ -1,13 +1,63 @@
 package com.home.quhong.quhong;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
-public class QuHong extends AppCompatActivity {
+import com.home.quhong.quhong.Local.fragments.LocalFragment;
+import com.home.quhong.quhong.My.fragments.MyFragment;
+import com.home.quhong.quhong.TV.fragments.TVFragment;
+import com.home.quhong.quhong.Video.fragments.VideoFragment;
+
+public class QuHong extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
+
+    private TVFragment mTvFragment;
+    private VideoFragment mVideoFragment;
+    private LocalFragment mLocalFragment;
+    private MyFragment mMyFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qu_hong);
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+        Fragment f = manager.findFragmentByTag("TV");
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.home_rg);
+        if (radioGroup != null) {
+            radioGroup.setOnCheckedChangeListener(this);
+        }
+        mTvFragment = new TVFragment();
+        mVideoFragment = new VideoFragment();
+        mLocalFragment = new LocalFragment();
+        mMyFragment = new MyFragment();
+        radioGroup.check(R.id.home_rb_tv);
+
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+        switch (checkedId){
+            case R.id.home_rb_tv:
+                ft.replace(R.id.home_fragment_contain,mTvFragment);
+                break;
+            case R.id.home_rb_video:
+                ft.replace(R.id.home_fragment_contain,mVideoFragment);
+                break;
+            case R.id.home_rb_local:
+                ft.replace(R.id.home_fragment_contain,mLocalFragment);
+                break;
+            case R.id.home_rb_my:
+                ft.replace(R.id.home_fragment_contain,mMyFragment);
+                break;
+        }
+        ft.commit();
     }
 }
