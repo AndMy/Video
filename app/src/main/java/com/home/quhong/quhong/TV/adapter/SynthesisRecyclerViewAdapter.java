2b758613cac45f7b6dfcffa8a12c73c63bed9f61
aserbao <abybxc@163.com>
2017-02-22 19:23:57 +0800

    Network data loading is completed, the page show success（网络数据加载完成，页面展示成功）

--- app/src/main/java/com/home/quhong/quhong/TV/adapter/SynthesisRecyclerViewAdapter.java
+++ app/src/main/java/com/home/quhong/quhong/TV/adapter/SynthesisRecyclerViewAdapter.java
@@ -1,13 +1,22 @@
 package com.home.quhong.quhong.TV.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.home.quhong.quhong.R;
+import com.home.quhong.quhong.TV.entity.Live;
 import com.home.quhong.quhong.TV.entity.LiveIndex;
+import com.home.quhong.quhong.TV.entity.PartitionSub;
+import com.home.quhong.quhong.TV.utils.ToastUtil;
 import com.home.quhong.quhong.TV.widght.banner.BannerEntity;
 import com.home.quhong.quhong.TV.widght.banner.BannerView;
 
@@ -17,8 +26,6 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-import static android.R.attr.id;
-
 /**
  * Created by aserbao on 2017/2/20.
  * e_mail:abybxc@163.com
@@ -26,12 +33,14 @@ import static android.R.attr.id;
  */
 
 public class SynthesisRecyclerViewAdapter extends RecyclerView.Adapter{
-    private Context mContext;
+    private Context context;
     private List<BannerEntity> banner;
     private List<Integer> liveSizes = new ArrayList<>();
     private LiveIndex liveIndex;
     private int entranceSize;
-    //综合页Banner
+    private static final int TYPE_ENTRANCE = 0;
+    private static final int TYPE_LIVE_ITEM = 1;
+    private static final int TYPE_PARTITION = 2;
     private static final int TYPE_BANNER = 3;
 
     private int[] entranceIconRes = new int[]{
@@ -42,13 +51,13 @@ public class SynthesisRecyclerViewAdapter extends RecyclerView.Adapter{
     };
 
     public SynthesisRecyclerViewAdapter(Context context) {
-        mContext = context;
+        this.context = context;
     }
     public void setLiveIndex(LiveIndex data)
     {
 
         this.liveIndex = data;
-        entranceSize = 4;
+        entranceSize = 1;
         int partitionSize = data.partitions.size();
 
         banner = new ArrayList<>();
@@ -69,17 +78,51 @@ public class SynthesisRecyclerViewAdapter extends RecyclerView.Adapter{
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
 
         View view;
+        switch (viewType){
+            case TYPE_ENTRANCE:
+                view = LayoutInflater.from(viewGroup.getContext())
+                        .inflate(R.layout.item_live_entrance, null);
+                return new LiveEntranceViewHolder(view);
+
+            case TYPE_BANNER:
                 view = LayoutInflater.from(viewGroup.getContext())
                         .inflate(R.layout.item_synthesis_banner, null);
                 return new SynthesisBannerViewHolder(view);
+            case TYPE_PARTITION:
+                view = LayoutInflater.from(viewGroup.getContext())
+                        .inflate(R.layout.item_live_partition_title, null);
+                return new LivePartitionViewHolder(view);
+            case TYPE_LIVE_ITEM:
+                view = LayoutInflater.from(viewGroup.getContext())
+                        .inflate(R.layout.item_live_partition, null);
+                return new LiveItemViewHolder(view);
+
+        }
+          return null;
 
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        position = -1 ;
+        position -= 1 ;
+        final Live item;
         if(holder instanceof SynthesisBannerViewHolder){
             ((SynthesisBannerViewHolder) holder).banner.delayTime(5).build(banner);
+        }else if(holder instanceof LivePartitionViewHolder){
+            PartitionSub partition = liveIndex.partitions.get(partitionCol(position)).partition;
+            ((LivePartitionViewHolder) holder).itemTitle.setText(partition.name);
+        }else if(holder instanceof LiveItemViewHolder){
+            item = liveIndex.partitions.get(partitionCol(position))
+                    .lives.get(position - 1 - entranceSize - partitionCol(position) * 10);
+            Glide.with(context)
+                    .load(item.cover.src)
+                    .centerCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .placeholder(R.drawable.bili_default_image_tv)
+                    .dontAnimate()
+                    .into(((LiveItemViewHolder) holder).itemLiveCover);
+            ((LiveItemViewHolder) holder).itemLiveTitle.setText(item.title);
+            ((LiveItemViewHolder) holder).itemLiveLayout.setOnClickListener(v -> ToastUtil.ShortToast("查看详情"));
         }
     }
 
@@ -101,22 +144,28 @@ public class SynthesisRecyclerViewAdapter extends RecyclerView.Adapter{
         if (position == 0){
             return TYPE_BANNER;
         }
-        // TODO: 2017/2/20 其他部分编写
-
-        return super.getItemViewType(position);
+        position -= 1;
+        if(position < entranceSize){
+            return TYPE_ENTRANCE;
+        }else if(ifPartitionTitle(position)){
+            return TYPE_PARTITION;
+        }else{
+            return TYPE_LIVE_ITEM;
+        }
     }
+
     public int getSpanSize(int pos)
     {
 
         int viewType = getItemViewType(pos);
         switch (viewType)
         {
-            /*case TYPE_ENTRANCE:
-                return 3;
-            case TYPE_LIVE_ITEM:
-                return 6;
+            case TYPE_ENTRANCE:
+                return 12;
             case TYPE_PARTITION:
-                return 12;*/
+                return 12;
+            case TYPE_LIVE_ITEM:
+                return 4;
             case TYPE_BANNER:
                 return 12;
         }
@@ -136,5 +185,51 @@ public class SynthesisRecyclerViewAdapter extends RecyclerView.Adapter{
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
+    }
+    static class LiveItemViewHolder extends RecyclerView.ViewHolder
+    {
+
+        @BindView(R.id.item_live_cover)
+        ImageView itemLiveCover;
+
+        @BindView(R.id.item_live_layout)
+        CardView itemLiveLayout;
+
+        @BindView(R.id.item_live_title)
+        TextView itemLiveTitle;
+        LiveItemViewHolder(View itemView)
+        {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+    static class LiveEntranceViewHolder extends RecyclerView.ViewHolder
+    {
+        LiveEntranceViewHolder(View itemView)
+        {
+            super(itemView);
+        }
+    }
+
+    static class LivePartitionViewHolder extends RecyclerView.ViewHolder
+    {
+        @BindView(R.id.item_live_partition_title)
+        TextView itemTitle;
+
+        LivePartitionViewHolder(View itemView)
+        {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+    private boolean ifPartitionTitle(int pos)
+    {
+        pos -= entranceSize;
+        return (pos % 10 == 0);
+    }
+    private int partitionCol(int pos)
+    {
+        pos -= entranceSize;
+        return pos / 10;
     }
 }
